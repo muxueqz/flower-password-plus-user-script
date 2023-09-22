bc80147ef3a295c37b178d2c02bb53b54a08627a
muxueqz <zhangmingyuan240@gmail.com>
2023-09-22 15:26:30 +0800

    set require version

--- flowerpassword.user.js
+++ flowerpassword.user.js
@@ -15,9 +15,9 @@
 // @match        http://*/*
 // @match        https://*/*
 // @require      https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js
-// @require      https://cdn.jsdelivr.net/npm/fpcode/dist/flowerpassword.umd.js
+// @require      https://cdn.jsdelivr.net/npm/fpcode@2.0.0/dist/flowerpassword.umd.js
 // @require      https://cdn.jsdelivr.net/npm/punycode@1.4.1/punycode.min.js
-// @require      https://cdn.jsdelivr.net/gh/gorhill/publicsuffixlist.js/publicsuffixlist.min.js
+// @require      https://cdn.jsdelivr.net/gh/gorhill/publicsuffixlist.js@1.0.0/publicsuffixlist.min.js
 // @resource     FP_STYLE https://cdn.jsdelivr.net/gh/xlsdg/flower-password-user-script/fp.min.css
 // @resource     PUBLIC_SUFFIX_LIST https://publicsuffix.org/list/public_suffix_list.dat
 // @run-at       document-end
@@ -28,69 +28,69 @@
 // @encoding     utf-8
 // ==/UserScript==
 
-(function () {
-  'use strict';
+;(function () {
+  'use strict'
 
-  var currentField = null;
+  var currentField = null
 
   function setupInputListeners() {
     function insideBox(e) {
-      return e.parents('#flower-password-input').length > 0;
+      return e.parents('#flower-password-input').length > 0
     }
 
-    var lstPublicSuffix = GM_getResourceText('PUBLIC_SUFFIX_LIST');
-    publicSuffixList.parse(lstPublicSuffix, punycode.toASCII);
+    var lstPublicSuffix = GM_getResourceText('PUBLIC_SUFFIX_LIST')
+    publicSuffixList.parse(lstPublicSuffix, punycode.toASCII)
 
-    var hostname = location.hostname.toLowerCase();
-    var domain = publicSuffixList.getDomain(hostname);
-    var suffix = publicSuffixList.getPublicSuffix(hostname);
+    var hostname = location.hostname.toLowerCase()
+    var domain = publicSuffixList.getDomain(hostname)
+    var suffix = publicSuffixList.getPublicSuffix(hostname)
 
     $(document).on('focus', 'input:password', function () {
       if (insideBox($(this))) {
-        return;
+        return
       }
 
-      lazyInject();
+      lazyInject()
 
       if (currentField && currentField.get(0) != this) {
-        $('#flower-password-password, #flower-password-key').val('');
+        $('#flower-password-password, #flower-password-key').val('')
       }
 
-      currentField = $(this);
+      currentField = $(this)
 
-      var offset = currentField.offset();
-      var height = currentField.outerHeight();
+      var offset = currentField.offset()
+      var height = currentField.outerHeight()
       $('#flower-password-input')
         .css({
           left: offset.left + 'px',
           top: offset.top + height + 'px',
         })
-        .show();
+        .show()
 
-      var code = '';
-      var key = domain.replace('.' + suffix, '') + code;
-      $('#flower-password-key').val(key);
-    });
+      var code = ''
+      var key = domain.replace('.' + suffix, '') + code
+      $('#flower-password-key').val(key)
+    })
 
     $(document).on('focus', 'input:not(:password)', function () {
       if (insideBox($(this))) {
-        return;
+        return
       }
-      $('#flower-password-input').hide();
-    });
+      $('#flower-password-input').hide()
+    })
   }
 
   function isInjected() {
-    return $('#flower-password-input').length > 0;
+    return $('#flower-password-input').length > 0
   }
 
   function lazyInject() {
     if (isInjected()) {
-      return;
+      return
     }
 
-    var style = GM_getResourceText('FP_STYLE');
-    GM_addStyle(style);
+    var style = GM_getResourceText('FP_STYLE')
+    GM_addStyle(style)
 
     var html =
       '<div id="flower-password-input" style="display: none;">' +
@@ -100,33 +100,33 @@
       '<br/>' +
       '<label for="flower-password-key">区分代号</label><input id="flower-password-key" name="flower-password-key" type="text" value=""/>' +
       '<p id="flower-password-hint">· 记忆密码：可选择一个简单易记的密码，用于生成其他高强度密码。<br/>· 区分代号：用于区别不同用途密码的简短代号，如淘宝账号可用“taobao”或“tb”等。<br/>· 按Enter键或Esc键关闭本窗口。<br/>· 花密官网地址：<a href="https://flowerpassword.com/" target="_blank">https://flowerpassword.com/</a></p>' +
-      '</div>';
-    $('body').append(html);
+      '</div>'
+    $('body').append(html)
 
     var onChange = function () {
-      var password = $('#flower-password-password').val();
-      var key = $('#flower-password-key').val();
-      var result = fpCode(password, key);
+      var password = $('#flower-password-password').val()
+      var key = $('#flower-password-key').val()
+      var result = fpCode(password, key)
       if (result) {
-        currentField.val(result);
-        GM_setClipboard(result);
+        currentField.val(result)
+        GM_setClipboard(result)
       }
-    };
+    }
 
     $('#flower-password-password, #flower-password-key')
       .change(onChange)
       .keyup(onChange)
       .keyup(function (e) {
         if (e.which === 13 || e.which === 27) {
-          currentField.focus();
-          $('#flower-password-input').hide();
+          currentField.focus()
+          $('#flower-password-input').hide()
         }
-      });
+      })
 
     $('#flower-password-close').click(function () {
-      $('#flower-password-input').hide();
-    });
+      $('#flower-password-input').hide()
+    })
   }
 
-  setupInputListeners();
-})();
+  setupInputListeners()
+})()
